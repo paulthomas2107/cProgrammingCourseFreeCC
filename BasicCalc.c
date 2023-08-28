a6008979a0fd12cb97abbd902b68bd471e5a456e
Paul Thomas <paulthomas2107@gmail.com>
2023-08-28 17:26:39 +0100

    More

--- BasicCalc.c
+++ BasicCalc.c
@@ -5,5 +5,13 @@ int main()
 {
     system("clear");
 
+    double num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+    printf("Answer: %f\n", num1 + num2);
+
     return 0;
 }
