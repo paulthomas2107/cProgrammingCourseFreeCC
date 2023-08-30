9884ee3620506009a1bffd8e238b7c0e1fc6d7e5
Paul Thomas <paulthomas2107@gmail.com>
2023-08-30 18:13:03 +0100

    More

--- Structs.c
+++ Structs.c
@@ -6,5 +6,23 @@ int main()
 {
     system("clear");
 
+    struct Student student1;
+    student1.id = 1001;
+    student1.age = 22;
+    student1.GPA = 3.2;
+    strcpy(student1.name, "Jim Henson"); // move text in
+    strcpy(student1.major, "Business");
+    printf("%.2f\n", student1.GPA);
+    printf("%s\n", student1.name);
+
+    struct Student student2;
+    student2.id = 1002;
+    student2.age = 21;
+    student2.GPA = 3.4;
+    strcpy(student2.name, "Pam Potts"); // move text in
+    strcpy(student2.major, "Art");
+    printf("%.2f\n", student2.GPA);
+    printf("%s\n", student2.name);
+
     return 0;
 }
---
+++ Structs.h
@@ -0,0 +1,9 @@
+#include <string.h>
+struct Student
+{
+    int id;
+    char name[50];
+    char major[50];
+    int age;
+    double GPA;
+};
