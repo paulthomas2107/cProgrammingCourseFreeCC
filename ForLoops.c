9fe8ca86400605eaf1ef6f5d51373ac5f7310619
Paul Thomas <paulthomas2107@gmail.com>
2023-08-30 20:18:03 +0100

    More

--- ForLoops.c
+++ ForLoops.c
@@ -5,9 +5,11 @@ int main()
 {
     system("clear");
 
-    for (int i = 0; i < 21; i++)
+    int nums[] = {1, 4, 5, 6, 7, 8, 10, 11, 217, 4, 2, 1, 222};
+
+    for (int i = 0; i < *(&nums + 1) - nums; i++) // counts thru array
     {
-        printf("%d\n", i);
+        printf("%d\n", nums[i]);
     }
 
     return 0;
